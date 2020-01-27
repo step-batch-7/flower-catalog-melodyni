245d1af6550c8925ba356ee0697ff4df9afceda1
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-01-27 12:54:55 +0530

    refactored code

--- server.js
+++ server.js
@@ -1,47 +1,10 @@
 const { Server } = require('net');
-const { readFileSync, existsSync, statSync } = require('fs');
 const { Request } = require('./lib/request');
-const { Response } = require('./lib/response');
-const { loadTemplate } = require('./lib/viewTemplates');
-
-const TEMPLATES = `${__dirname}/templates`;
-const STATIC_FOLDER = `${__dirname}/public`;
-const CONTENT_TYPES = {
-  txt: 'text/plain',
-  html: 'text/html',
-  css: 'text/css',
-  js: 'application/javascript',
-  json: 'application/json',
-  gif: 'image/gif',
-};
-
-const serveFileNotFoundPage = function (path) {
-  const response = new Response();
-  const html = loadTemplate(path, response);
-  response.setHeader('Content-Type', html.length);
-  response.addContent(html);
-  return response;
-}
-
-const handleGet = function (url) {
-  const path = `${STATIC_FOLDER}/${url}`;
-  const doesFileExist = existsSync(path) && statSync(path).isFile();
-  if (!doesFileExist) {
-    return serveFileNotFoundPage('fileNotFound.html');
-  }
-  const extension = path.split('.').pop();
-  const content = readFileSync(path);
-  const response = new Response();
-  response.setSuccessCode();
-  response.setHeader('Content-Type', CONTENT_TYPES[extension]);
-  response.setHeader('Content-Length', content.length);
-  response.addContent(content);
-  return response;
-}
-
+const { handleGet } = require('./lib/handlers');
 const pickHandler = function (method) {
   const handlers = {
     GET: handleGet
+    // POST: handlePost
   }
   return handlers[method];
 }
