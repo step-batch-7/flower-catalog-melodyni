05ed18fc425ad667850960a754f2ef07bb6aaaef
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-01-30 12:52:50 +0530

    refactored code

--- public/js/wateringJar.js
+++ public/js/wateringJar.js
@@ -1,7 +1,7 @@
 const hideJar = function () {
   const jar = document.querySelector('.jar');
-  jar.style.display = "none";
-  setTimeout(() => { jar.style.display = "block"; }, 1000);
-}
-
-
+  jar.style.display = 'none';
+  setTimeout(() => {
+    jar.style.display = 'block';
+  }, 1000);
+};
